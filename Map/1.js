96f622f7c3184d219f6310b6507a5b89eb8355aa
keal khondekar <kewalkhondekar25@gmail.com>
2024-02-01 23:57:16 +0530

    feat - map key method added

--- Map/1.js
+++ Map/1.js
@@ -1,5 +1,10 @@
 const collection = new Map();
 collection.set("TV", "Sony Bravia");
 collection.set(1,"Apple macbook");
+for(var key of collection.keys()){
+    console.log(key);
+}
+
+
 console.log(collection.get("TV"));
 console.log(collection.get(1));
