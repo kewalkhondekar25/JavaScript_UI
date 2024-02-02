4c26e6b7025db73074640db15ec09aa6d9d7af5e
keal khondekar <kewalkhondekar25@gmail.com>
2024-02-03 00:17:51 +0530

    refactor - date type

--- src/DateType/3.js
+++ src/DateType/3.js
@@ -3,3 +3,4 @@ let week = ["sun", "mon", "tue", "wed", "thur", "fri", "sat"];
 let month = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
 
 console.log(`today is ${date.getDate()} ${month[date.getMonth()]} ${date.getFullYear()} ${week[date.getDay()]}`);
+
