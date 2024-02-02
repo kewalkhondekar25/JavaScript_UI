6419a6fab073184369c8fea266927a08d8e4c267
keal khondekar <kewalkhondekar25@gmail.com>
2024-02-03 00:05:09 +0530

    feat - all getDate methods added

--- src/DateType/3.js
+++ src/DateType/3.js
@@ -1,3 +1,5 @@
 let date = new Date();
 let week = ["sun", "mon", "tue", "wed", "thur", "fri", "sat"];
-console.log(`today is ${week[date.getDay()]}`);
+let month = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
+
+console.log(`today is ${date.getDate()} ${month[date.getMonth()]} ${date.getFullYear()} ${week[date.getDay()]}`);
