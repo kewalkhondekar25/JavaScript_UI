4eee0aad65f4c703c101fda96f7c7883a6d9d4a7
keal khondekar <kewalkhondekar25@gmail.com>
2024-02-02 23:55:04 +0530

    feat - getday added

--- src/DateType/2.js
+++ src/DateType/2.js
@@ -1,3 +1,3 @@
 let dob = new Date();
 console.log(dob.toLocaleDateString());
-console.log(`date: ${dob.getDate()} month: ${dob.getMonth()} `);
+console.log(`date: ${dob.getDate()} month: ${dob.getMonth()} weekday: ${dob.getDay()} `);
