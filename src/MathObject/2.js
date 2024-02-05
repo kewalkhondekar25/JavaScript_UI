51da6ccb520ad47cd81be8039dd19eae01b7d4a5
keal khondekar <kewalkhondekar25@gmail.com>
2024-02-05 23:51:21 +0530

    feat - OTP method added

---
+++ src/MathObject/2.js
@@ -0,0 +1,11 @@
+const genOtp = () => {
+    const a = Math.random() * 10;
+    const b = Math.random() * 10;
+    const c = Math.random() * 10;
+    const d = Math.random() * 10;
+
+    console.log(`OTP: ${Math.round(a)} ${Math.round(b)} ${Math.round(c)} ${Math.round(d)}`);
+
+}
+
+genOtp();
