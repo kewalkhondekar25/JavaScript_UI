c007854e3acc640e9e9e4a2428ba808f50b98c73
keal khondekar <kewalkhondekar25@gmail.com>
2024-02-10 12:57:52 +0530

    feat - maps entries method added

--- src/Map/1.js
+++ src/Map/1.js
@@ -1,12 +1,16 @@
 const collection = new Map();
 collection.set("TV", "Sony Bravia");
 collection.set(1,"Apple macbook");
-collection.delete(1);
+// collection.delete(1);
 for(var key of collection.keys()){
     console.log(key);
 }
 for(var key of collection.values()){
     console.log(key);
+}
+
+for(var res of collection.entries()){
+    console.log(`entries: ${res}`);
 }
 
 console.log(collection.get("TV"));
