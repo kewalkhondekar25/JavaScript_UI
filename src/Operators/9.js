b10c9b1b4a26a1a38d4ae5ad38fc501f1419ed4b
keal khondekar <kewalkhondekar25@gmail.com>
2024-02-11 16:35:31 +0530

    feat - conditional operators added

---
+++ src/Operators/9.js
@@ -0,0 +1,15 @@
+//conditional operator
+let x = 10;
+let y = "10";
+
+if(x == y){
+    console.log("equal");
+}else{
+    console.log("not equal");
+}
+
+if(x === y){
+    console.log("equal");
+}else{
+    console.log("not equal");
+}
