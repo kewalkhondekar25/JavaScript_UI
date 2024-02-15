a44fe1bfb0887f4c685d3a14322338e3400c43d7
keal khondekar <kewalkhondekar25@gmail.com>
2024-02-15 23:07:09 +0530

    feat - callback function added

---
+++ src/Functions/3.js
@@ -0,0 +1,10 @@
+function loginUser(userName, password, success, failure){
+    if(userName == "arjun" && password == "arjun11")
+    {
+        success();
+    }else{
+        failure();
+    }
+};
+
+loginUser("arjun11", "arjun11", function(){console.log("login success")}, function(){console.log("invalid credentials")});
