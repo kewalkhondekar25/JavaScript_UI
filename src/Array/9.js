38dcc39fb6eaa02dfae8ae09ed123756463fbeff
keal khondekar <kewalkhondekar25@gmail.com>
2024-02-26 20:30:27 +0530

    feat - forEach for loop added

---
+++ src/Array/9.js
@@ -0,0 +1,17 @@
+//for loop
+
+const arr = [1, 2, 3, 4, 5];
+//index       0 1  2  3  4
+
+
+//i = 3         3  <     5       3
+for( let i = 0; i < arr.length; i++){
+    //              3
+    console.log(arr[i]);
+}
+//1
+//2
+//3
+//4
+
+arr.forEach( item => console.log(item))
