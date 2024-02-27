7f24f37ab10f021b6758e482c8e2cf2a229e460d
keal khondekar <kewalkhondekar25@gmail.com>
2024-02-27 16:34:19 +0530

    feat - promise resolve & rejct added

--- src/Promise/1.js
+++ src/Promise/1.js
@@ -18,3 +18,37 @@ new Promise((resolve, reject) => {
 .then(() => {
     console.log("promise 2 consumed");
 })
+
+new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("Async task 3 completed");
+        resolve({user: "arjun", email: "arjun@icloud.com"});
+    }, 3000);
+})
+.then((user) => {
+    console.log("promise 3 consumed");
+    console.log(user);
+})
+
+new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("Async task 4 completed");
+        let error = true;
+        if(error)
+        {
+            resolve({tech: "react", category: "UI"});
+        }else
+        {
+            reject("ERROR: something went wrong");
+        }
+    }, 4000);
+})
+.then((user) => {
+    return user.tech
+})
+.then((tech) => {
+    console.log(`tech: ${tech}`);
+})
+.catch((error) => {
+    console.log(error);
+})
