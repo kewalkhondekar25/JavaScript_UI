0a3fd4578f6b02c91d883f738fa213b33cf9d0eb
keal khondekar <kewalkhondekar25@gmail.com>
2024-02-27 16:12:00 +0530

    feat - promise resolve added

--- src/Promise/1.js
+++ src/Promise/1.js
@@ -1,11 +1,20 @@
 const promiseOne = new Promise((resolve, reject) => {
     //do async task
     setTimeout(() => {
-        console.log("Async task is completed");
+        console.log("Async task 1 is completed");
         resolve()
     }, 1000);
 })
-
 promiseOne.then(() => {
     console.log("promise one consumed");
+})
+
+new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("Async task 2 is completed");
+        resolve();
+    }, 2000);
+})
+.then(() => {
+    console.log("promise 2 consumed");
 })
