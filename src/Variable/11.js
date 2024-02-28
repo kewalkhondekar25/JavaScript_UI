3b8950da10cc03498f625737c733b8cfc53b2693
keal khondekar <kewalkhondekar25@gmail.com>
2024-02-28 17:01:42 +0530

    feat - TDZ added

---
+++ src/Variable/11.js
@@ -0,0 +1,8 @@
+let a = 69;
+
+if(true)
+{
+    let a = 99;
+    console.log(a);
+}
+console.log(a);
---
+++ src/Variable/12.js
@@ -0,0 +1,8 @@
+const print = () => {
+    console.log(c);
+    var c = 20;
+    // let a = 69;
+    // const b = 10;
+}
+
+print();
