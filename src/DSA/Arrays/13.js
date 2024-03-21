4e72a9a634f5b32f8f3e4dbf99152f1a0dcd4f09
keal khondekar <kewalkhondekar25@gmail.com>
2024-03-21 19:50:54 +0530

    feat - arrays spread & rest operator added

--- src/DSA/Arrays/13.js
+++ src/DSA/Arrays/13.js
@@ -5,3 +5,9 @@ let arr2 = [69, 70, 71];
 
 let finalArr = [...arr1, ...arr2];
 console.log(finalArr);
+
+//rest operator
+const sum = (...nums) => {  //rest operators/ rest param
+    return nums
+}
+console.log(sum(arr1, arr2, 'hi'));
