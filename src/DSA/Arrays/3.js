058fc5ea3877fcc8beb2d4ed403d2504eb5f45e9
keal khondekar <kewalkhondekar25@gmail.com>
2024-03-21 15:23:16 +0530

    feat - Arrays unshift & shift method added

---
+++ src/DSA/Arrays/3.js
@@ -0,0 +1,12 @@
+//add or remove 1st element
+
+let names = ['john', 'david', 'sam'];
+console.log(names);
+
+//unshift adds element to top
+names.unshift('harry');
+console.log(names);
+
+//shift removes top element
+names.shift();
+console.log(names);
