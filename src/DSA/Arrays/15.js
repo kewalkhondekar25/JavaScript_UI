13f7cb10c972535628930766778ce6a6cdd7fc56
keal khondekar <kewalkhondekar25@gmail.com>
2024-03-21 20:09:11 +0530

    feat - arrays fill added

--- src/DSA/Arrays/15.js
+++ src/DSA/Arrays/15.js
@@ -6,3 +6,6 @@ console.log(fruits);
 let sliceFruits = fruits.slice(0, 2);
 console.log(sliceFruits);
 
+let nums = [1, 2, 3, 4, 5];
+let sliceNum = nums.slice(-1);
+console.log(sliceNum);
---
+++ src/DSA/Arrays/16.js
@@ -0,0 +1,7 @@
+//fill
+
+let oldArr = [1, 2, 3, 4, 5];
+console.log(oldArr);
+
+oldArr.fill(69);
+console.log(oldArr);
