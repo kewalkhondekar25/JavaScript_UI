1a9fb6d5f59579d9b4e4c2112f80c0a75f3c2e1d
keal khondekar <kewalkhondekar25@gmail.com>
2024-03-21 14:44:18 +0530

    feat - Array push & pop method added

---
+++ src/DSA/Arrays/2.js
@@ -0,0 +1,15 @@
+//add or remove methods - push & pop
+
+let arr = ['apple', 'banana', 'cherry'];
+console.log(arr);
+
+//pop - adds as a last element
+arr.push('dragon fruit');
+console.log(arr);
+
+//pop - removes last element
+arr.pop();
+console.log(arr);
+
+arr.pop();
+console.log(arr);
