216624b5ae8208dce1a3a302d86b57dabb105a92
keal khondekar <kewalkhondekar25@gmail.com>
2024-03-21 18:15:23 +0530

    feat - arrays map added

--- src/DSA/Arrays/7.js
+++ src/DSA/Arrays/7.js
@@ -1,2 +1,11 @@
 //inbuilt loop
 
+//map
+
+let arr = [1, 2, 3, 4, 5];
+console.log(arr);
+
+arr.map((item, index, array) => console.log(item, index, array));
+
+let newArr = arr.map(item => item * 69);
+console.log(newArr);
