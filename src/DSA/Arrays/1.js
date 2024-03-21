9beea406980adec7cd4e807b66c2b8cc155b14a9
keal khondekar <kewalkhondekar25@gmail.com>
2024-03-21 14:37:30 +0530

    feat - Array data structures added

---
+++ src/DSA/Arrays/1.js
@@ -0,0 +1,18 @@
+//array declaration
+let arr = new Array();
+
+let arr2 = [];
+
+let fruits = ['apple', 'banana', 'cherry'];
+
+console.log(fruits[0]);
+
+let info = {
+    name: 'john',
+    email: 'john@google.com'
+};
+
+let arr3 = ['harry', 'david', info];
+
+console.log(arr3.length);
+console.log(arr3[2].email);
