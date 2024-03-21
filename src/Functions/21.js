e5cec6c0019c9701df5e8af06456695800595265
keal khondekar <kewalkhondekar25@gmail.com>
2024-03-21 17:22:55 +0530

    feat - callBack example function added

--- src/Functions/21.js
+++ src/Functions/21.js
@@ -13,3 +13,15 @@ x(function y(){
     //y is called callBack func
     console.log('y');
 });
+
+//callback example
+const Login = (user, pwd, success, failure) => {
+    if(user === 'john' && pwd === 'john123')
+    {
+        success();
+    }else
+    {
+        failure();
+    }
+}
+Login('john123', 'john123', () => console.log('success'), () => console.log('failure'));
