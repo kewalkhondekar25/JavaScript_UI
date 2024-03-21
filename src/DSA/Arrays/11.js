a4139dab2ba7107bcf9670fe13feaba42f4404f6
keal khondekar <kewalkhondekar25@gmail.com>
2024-03-21 19:38:23 +0530

    feat - arrays every & find method added

---
+++ src/DSA/Arrays/11.js
@@ -0,0 +1,6 @@
+//every
+
+let arr = [1, 1, 1];
+
+let res = arr.every(item => item < 2);
+console.log(res);
---
+++ src/DSA/Arrays/12.js
@@ -0,0 +1,6 @@
+let arr = [1, 2, 3, 4, 5]
+
+let res = arr.find(item => item > 3);
+// let res = arr.find(item => item > 5); - undefined
+
+console.log(res);
