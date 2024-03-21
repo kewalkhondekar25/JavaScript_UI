21e38fc2c629fcc3306a3230ea4194c667b514a8
keal khondekar <kewalkhondekar25@gmail.com>
2024-03-21 23:49:45 +0530

    feat - arrays flat added

---
+++ src/DSA/Arrays/17.js
@@ -0,0 +1,10 @@
+//findIndex
+let nums = [1, 2, 3, 4, 5];
+
+let res = nums.findIndex(item => item === 3);
+console.log(res);
+
+//flat
+let arr = [1, 2, [3, 4], 5]
+let flat = arr.flat();
+console.log(flat);
