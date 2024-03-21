b5bf0ee6c545e397267e296ee9966c885b94ea42
keal khondekar <kewalkhondekar25@gmail.com>
2024-03-22 00:00:29 +0530

    feat - arrays reverse & sort added

---
+++ src/DSA/Arrays/18.js
@@ -0,0 +1,15 @@
+//reverse
+let nums = [1, 2, 3, 4, 5];
+console.log(nums);
+
+let rev = nums.reverse();
+console.log(rev);
+
+//sort
+let alpha = [69, 77, 99, 25];
+console.log(alpha);
+
+let revAlpha = alpha.sort((a, b) => a - b); //ascending sort
+// let revAlpha = alpha.sort((a, b) => b - a); //descending sort
+
+console.log(revAlpha);
